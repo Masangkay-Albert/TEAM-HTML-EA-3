a50d0bfa427394d15a897923995efa263c521993
James <jamessaluta2003@gmail.com>
2026-09-09 19:25:05 +0800

    add james' item 2

--- TEAM-HTML/src/app/app.ts
+++ TEAM-HTML/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Luna } from './luna/luna'; 
 import { Masangkay } from './masangkay/masangkay';
+import { Saluta } from './saluta/saluta';
 
 @Component({
   selector: 'app-root',
-  imports: [Masangkay, Luna],
+  imports: [Masangkay, Luna, Saluta],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- TEAM-HTML/src/app/saluta/saluta.ts
+++ TEAM-HTML/src/app/saluta/saluta.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-saluta',
+  
   imports: [],
   templateUrl: './saluta.html',
   styleUrl: './saluta.scss',
 })
-export class Saluta {}
+export class Saluta {
+  name = signal('James Saluta');
+}
