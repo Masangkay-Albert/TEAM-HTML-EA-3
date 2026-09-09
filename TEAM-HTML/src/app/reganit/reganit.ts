211eb9ab349e6e22cc1c2537b085cc3259b20e45
Reganit-JohnRaphael <johnraphaelreganit23@gmail.com>
2026-09-09 18:10:23 +0800

    Add Component Reganit

---
+++ TEAM-HTML/src/app/reganit/reganit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reganit',
+  imports: [],
+  templateUrl: './reganit.html',
+  styleUrl: './reganit.scss',
+})
+export class Reganit {
+
+}
