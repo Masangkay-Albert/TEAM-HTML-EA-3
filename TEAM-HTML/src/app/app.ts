5f569a20dcea2414c489e63c57049c11d8018a35
Reganit-JohnRaphael <johnraphaelreganit23@gmail.com>
2026-09-09 18:19:13 +0800

    Create signal and modify template

--- TEAM-HTML/src/app/app.ts
+++ TEAM-HTML/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { Masangkay } from './masangkay/masangkay';
 
+
+
+import { Reganit } from './reganit/reganit'; 
+
 @Component({
   selector: 'app-root',
-  imports: [Masangkay],
+  imports: [Masangkay, Reganit],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- TEAM-HTML/src/app/reganit/reganit.ts
+++ TEAM-HTML/src/app/reganit/reganit.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-reganit',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './reganit.scss',
 })
 export class Reganit {
-
+name = signal ("John Raphael");
 }
