d1f4dc2a9887b8b01b9c3228e0925f0559d94562
alarie <alariesomuelo@gmail.com>
2026-09-09 21:11:19 +0800

    add somuelo's first item

---
+++ TEAM-HTML/src/app/somuelo/somuelo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-somuelo',
+  imports: [],
+  templateUrl: './somuelo.html',
+  styleUrl: './somuelo.scss',
+})
+export class Somuelo {}
