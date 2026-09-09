54cf72348af34142e7f25a51b08a75714fb66b19
Reganit-JohnRaphael <johnraphaelreganit23@gmail.com>
2026-09-09 20:32:21 +0800

    add reganit item 2

--- TEAM-HTML/src/app/app.ts
+++ TEAM-HTML/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Luna } from './luna/luna'; 
 import { Masangkay } from './masangkay/masangkay';
 import { Saluta } from './saluta/saluta';
+import { Reganit } from './reganit/reganit';
 
 @Component({
   selector: 'app-root',
-  imports: [Masangkay, Luna, Saluta],
+  imports: [Masangkay, Luna, Saluta, Reganit],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
