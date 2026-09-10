14e668847889bf56ce13e355286c87ca558a5b3f
Masangkay-JohnAlbert <masangkayjohnalbert1216@gmail.com>
2026-09-10 22:57:54 +0800

    proper code, based on the coding guidelines

--- TEAM-HTML/src/app/masangkay/masangkay.ts
+++ TEAM-HTML/src/app/masangkay/masangkay.ts
@@ -12,10 +12,13 @@ import { MatCardModule } from '@angular/material/card';
 export class Masangkay {
   name = signal('John Albert');
   photoPath = signal('assets/masangkay/masangkay.jpg');
-  showDescription = signal(false);
-  description = signal('John Albert is a 4th year college student at De La Salle University-Dasmarinas, taking a Bachelor of Science in Information Technology.');
+  isDescriptionVisible = signal(false);
+  description = signal(
+    'John Albert is a 4th year college student at De La Salle University'
+      + '-Dasmarinas, taking a Bachelor of Science in Information Technology.'
+  );
 
-  toggleDescription() {
-    this.showDescription.set(!this.showDescription());
+  toggleDescription(): void {
+    this.isDescriptionVisible.set(!this.isDescriptionVisible());
   }
 }
