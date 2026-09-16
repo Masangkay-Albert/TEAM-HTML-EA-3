ed5ed24f224bf6fb108efd4ac8f8892ddc2c9678
Luna-GracyMae <gracy.dlsud@gmail.com>
2026-09-16 09:23:13 +0800

    add MemberPreview component to shared/ directory

--- TEAM-HTML/src/app/luna/luna.ts
+++ TEAM-HTML/src/app/luna/luna.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MemberPreview } from '../shared/member-preview/member-preview';
 
 
 @Component({
   selector: 'app-luna',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MemberPreview],
   templateUrl: './luna.html',
   styleUrl: './luna.scss',
 })
+
 export class Luna {
   name = signal ("Gracy Luna");
   photoPath = signal('assets/masangkay/luna/gracy.jpg');
@@ -18,8 +20,10 @@ export class Luna {
       +' creative activities, and could probably love her course even more '
       +'if her future workplace is at an airport.'
     );
-
-  toggleDescription(): void {
-    this.isDescriptionVisible.set(!this.isDescriptionVisible());
-  }
+    
+  avatarPath = signal('assets/masangkay/luna/sunnies.jpg');
+  cardImagePath = signal('assets/masangkay/luna/hachiware.jpg');
+  cardTitle = signal('Happy Meal Hachiware');
+  cardSubtitle = signal('McDonald\'s x Chiikawa');
+  cardText = 'A tiny Hachiware companion from the Happy Meal collection.';
 }
